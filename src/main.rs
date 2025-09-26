d14d95eed894d8cee2c3f957cb2426fd177b25eb
wayne <sodeh138@gmail.com>
2025-09-26 16:13:13 +0100

    new static route added

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,44 @@
-use axum::{Router, routing::get, extract::Query, response::IntoResponse};
+use axum::{
+  extract::{Path, Query}, response::IntoResponse, routing::{get, get_service}, Router
+};
 use std::net::SocketAddr;
 use serde::Deserialize;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 
 async fn main(){
-  let route = Router::new().route("/", get(handler_func));
-  let addr = SocketAddr::from(([127, 0, 0 , 1], 3000));
-  println!("the server is running at port: {}", addr);
-
+  let app = Router::new()
+  .merge(router_handlers())
+  .fallback_service(static_route());
+  let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
   let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
-  axum::serve(listener, route.into_make_service()).await.unwrap();
+
+  axum::serve(listener, app.into_make_service()).await.unwrap();
+}
+
+fn router_handlers() -> Router{
+  Router::new()
+  .route("/home", get(user_home_profiler))
+  .route("/home/{id}", get(user_id_resolver))
 }
 #[derive(Debug, Deserialize)]
-struct Userinfo{
-  username: Option<String>,
+
+struct Newuserlaucher{
+  name: Option<String>
+}
+async fn user_home_profiler(Query(payloader):Query<Newuserlaucher>)-> impl IntoResponse{
+   match payloader.name {
+       Some(n)=> format!("hello, distinguished {n}"),
+       None=>"hello random user, please sign in".to_string()
+   }
 }
 
-async fn handler_func(Query(params):Query<Userinfo>)-> impl IntoResponse{
-    match params.username{
-      Some(n)=> format!("hello, {n}, welcome!"),
-      None => "hello random user".to_string(),
-    }
+async fn user_id_resolver(Path(user_id): Path<String>)-> impl IntoResponse{
+  format!("hello user{}", user_id)
 }
+
+fn static_route()->Router{
+  Router::new().nest_service("/", get_service(ServeDir::new("./assets")))
+}
+
